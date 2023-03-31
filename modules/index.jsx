264fd04cfff316d8a8d28b5e89207e024e28f0ee
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2023-04-01 00:24:00 +0530

    first commit

---
+++ modules/index.jsx
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./Navbar";
+export { default as Body } from "./Body";
+export { default as WorkExperienceComponent } from './Cards/WorkExperience';
+export { default as ProjectsComponent } from "./Cards/Projects";
+export { default as SkillsComponent } from "./Cards/Skill";
+export { default as SocialMediaComponent } from "./Cards/SocialMedia";
+export { default as Divider } from "./UI/Divider";
---
+++ utils/data.js
@@ -0,0 +1,128 @@
+const portfolioData = {
+	name: "Shrey vijayvargiya",
+	heading: "this is Shrey",
+	description: `Developer &  Writer ✍️`,
+	caption: "I am Full Stack developer with 4 years of experience in developing mobile and desktop application with frontend being my primary strength",
+	workExperience: {
+		content: [
+			{
+				name: "Full Stack Developer",
+				date: "Present",
+				content:
+					"Developing and Running Full Stack website iHateReading along with Writing Blogs and Making online content",
+			},
+			{
+				name: "Senior Frontend Developer - Koo",
+				date: "August 2021 - September 2022",
+				content: "Lead frontend team of 6 members. My job is to develop and manage Koo website along with dashboard or admin panel for non-technical team to manage koos users.",
+			},
+			{
+				name: "Mobile App Developer - CoinSwitch",
+				date: "March 20 - jan 21",
+				content:
+					"Developed cryptocurrency trading application for millions of users. Successfully improved app(APK) bundle size by 40%. Increased Application runtime speed by 25%. Integration of GraphQL to enhance performance by 50%. Implementation of Graphs, Flatlists, Animations Firebase and Amplitude software tools.",
+			},
+			{
+				name: "Frontend Developer - Cogoport",
+				date: "April 19- Feb 20",
+				content: `Developed React UI library of 72 components (https:// nautical.cogoport.com). Developed the packages like React reusable hooks,React realtime editable Spreadsheet, CMS(Content management system) & UI Library. Worked on Babel, Webpack, Next JS, Redux, Gatsby`,
+			},
+		],
+	},
+	contactDetails: {
+		content: [
+			{
+				name: "Email",
+				link: "https://mailtoshreyvijayvargiya26@gmail.com",
+			},
+			{
+				name: "Phone number",
+				link: "+91-7030226230",
+			},
+		],
+	},
+	projects: {
+		content: [
+			{
+				name: "Crypto Exchange platform, Coinswtich",
+				link: "https://coinswitch.co/",
+			},
+			{
+				name: "Koo, micro blogging application",
+				link: "https://kooapp.com",
+			},
+			{
+				name: "Find Jobs Portals",
+				link: "https://findjobsportals.vercel.app/",
+			},
+			{
+				name: "Tech platform, Learn how products are developed, iHateReading",
+				link: "https://ihatereading.in",
+			},
+			{
+				name: "Etherscan clone",
+				link: "https://eth-txns.vercel.app/",
+			},
+			{
+				name: "Solana NFT marketplace, NFT marketplace",
+				link: "https://www.solana-nft-marketplace-pi.vercel.app/",
+			},
+			{
+				name: "CryptStream",
+				link: "https://www.solana-nft-marketplace-pi.vercel.app/",
+			},
+		],
+	},
+	education: {
+		content: [
+			{
+				name: "Masters in Biotechnology, BITS Pilani University",
+				timeline: "2015-2019",
+			},
+		],
+	},
+	skills: {
+		content: [
+			"Javascript",
+			"React",
+			"React Native",
+			"Firebase",
+			"Mongo DB",
+			"Express JS",
+			"SQL",
+		],
+	},
+	socialMedia: {
+		content: [
+			{
+				name: "Github",
+				link: "https://github.com/shreyvijayvargiya",
+			},
+			{
+				name: "Twitter",
+				link: "https://twitter.com/treyvijay",
+			},
+			{
+				name: "LinkedIn",
+				link: "https://www.linkedin.com/in/shrey-vijayvargiya-b62a3a105/",
+			},
+			{
+				name: "Youtube",
+				link: "https://youtube.com/@shreyvijay",
+			},
+			{
+				name: "DevTo",
+				link: "",
+			},
+			{
+				name: "Hashnode",
+				link: "",
+			},
+			{
+				name: "Instagram",
+				link: "",
+			},
+		],
+	},
+};
+export default portfolioData;
