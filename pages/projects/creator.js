251694bedd168884990f1b944b562493b86ed3d0
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-08-13 18:09:44 +0530

    framer removed!!

--- pages/projects/creator.js
+++ pages/projects/creator.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Avatar, Button, Textarea, TextInput } from "@mantine/core";
 import { useForm } from "react-hook-form";
-import { motion } from "framer-motion";
 import gsap from "gsap";
 import {
 	TwitterIcon,
