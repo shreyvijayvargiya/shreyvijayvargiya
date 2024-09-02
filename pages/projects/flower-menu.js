07f8d6d11caa3008097e483f6668c76329f89a95
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-02 17:30:24 +0530

    flower menu is completed!!

--- pages/projects/flower-menu.js
+++ pages/projects/flower-menu.js
@@ -6,6 +6,7 @@ import {
 	Settings,
 	MenuIcon,
 	CrossIcon,
+	PhoneCall,
 } from "lucide-react";
 import { gsap } from "gsap";
 
@@ -16,16 +17,17 @@ const CircleMenu = () => {
 		{ Icon: Laptop2Icon, label: "Product" },
 		{ Icon: PersonStandingIcon, label: "About" },
 		{ Icon: Settings, label: "Settings" },
+		{ Icon: PhoneCall, label: "Phone" },
 	];
 
 	const handleMenuClick = () => {
 		setIsOpen(!isOpen);
-    	gsap.to(".container", {
-				rotation: isOpen ? "+=720" : "0",
-				scale: isOpen ? 0 : 1,
-				duration: 1,
-				stagger: 0.2,
-			});
+		gsap.to(".container", {
+			rotation: isOpen ? "+=720" : "0",
+			scale: isOpen ? 0 : 1,
+			duration: 1,
+			stagger: 0.2,
+		});
 		gsap.to(".circle-item", {
 			rotation: isOpen ? "+=360" : "0",
 			scale: isOpen ? 0 : 1,
@@ -34,6 +36,8 @@ const CircleMenu = () => {
 		});
 	};
 
+	const numberOfItems = circleItems.length;
+	const angle = 360 / numberOfItems;
 	return (
 		<div className="relative flex justify-center items-center h-screen">
 			<div className="relative flex justify-center items-center w-64 h-64">
@@ -43,9 +47,9 @@ const CircleMenu = () => {
 					style={{ zIndex: 1 }}
 				>
 					{isOpen ? (
-						<CrossIcon size={20} color="white" />
+						<CrossIcon size={18} color="white" />
 					) : (
-						<MenuIcon size={20} color="white" />
+						<MenuIcon size={18} color="white" />
 					)}
 					<span className="text-xs text-gray-100 group-hover:inline-block group-hover:ml-2 hidden">
 						{isOpen ? "Click to Close" : "Click to Expand"}
@@ -53,16 +57,16 @@ const CircleMenu = () => {
 				</div>
 
 				<div
-					className={`container absolute flex justify-center items-center w-full h-full`}
+					className={`container absolute flex justify-center items-center w-full h-full border-2 p-10 rounded-full`}
 				>
 					{circleItems.map((item, index) => (
 						<div
 							key={index}
-							className="circle-item absolute flex items-center justify-center cursor-pointer rounded-md"
+							className="circle-item absolute cursor-pointer rounded-md"
 							style={{
-								transform: `rotate(${index * 90}deg) translateX(100px) rotate(${
-									index * 90
-								}deg)`,
+								transform: `rotate(${
+									index * angle
+								}deg) translateX(150px) rotate(${-index * angle}deg)`,
 								transition: "transform 0.5s ease",
 							}}
 						>
