bc2173028d0a045319533dc18a106942ce059feb
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-05 01:07:39 +0530

    this makes more sense!!

--- pages/projects/100-character.js
+++ pages/projects/100-character.js
@@ -13,28 +13,36 @@ const HundredCharacter = () => {
 	return (
 		<div className="h-screen w-full flex justify-center items-center">
 			<div className="w-96 overflow-hidden">
-				<div className="relative flex gap-4 items-center justify-end my-3 h-16 overflow-x-scroll w-full border border-gray-200 rounded-xl">
+				<div className="relative flex gap-4 items-center justify-end my-3 h-12 overflow-x-scroll w-full border border-gray-200 rounded-2xl">
 					<div
-						className={`absolute top-0 left-0 bottom-0 flex justify-center items-center rounded-l-xl rounded-r-none h-16 bg-green-200 z-20 transition-all duration-300 ease-in-out`}
+						className={`absolute top-0 left-0 bottom-0 flex justify-center items-center rounded-l-2xl rounded-r-none h-12 bg-green-200 z-20 transition-all duration-300 ease-in-out`}
 						style={{ width: progressWidth + "%" }}
 					>
-						{progressWidth > 20 ? "Goals" : null}
+						{Math.round(progressWidth) > 4 && Math.round(progressWidth)}
 					</div>
 					<div
-						className="relative h-16 flex gap-2 bg-red-200 justify-around items-center z-0 rounded-r-xl rounded-l-none transition-all duration-300 ease-in-out w-full"
+						className="relative h-12 flex gap-2 bg-red-200 justify-around items-center z-0 rounded-r-2xl rounded-l-none transition-all duration-300 ease-in-out w-full"
 						style={{ width: 100 - progressWidth + "%" }}
 					>
-						{progressWidth > 20 ? "Danger" : "Procastination"}
+						{Math.round(100 - progressWidth)}
 					</div>
 				</div>
-
 				<textarea
-					className="p-4 border-2 border-gray-200 outline-none focus:border-black rounded-xl w-full min-w-md max-w-xl"
+					className="p-6 border border-gray-50 outline-none focus:border-gray-50 w-full min-w-md max-w-xl"
 					placeholder="100 character limit"
 					onChange={handleChange}
 					maxLength={100}
 					value={val}
-					rows={3}
+					rows={4}
+					style={{
+						position: "relative",
+						padding: "1rem",
+						border: "2px solid transparent",
+						borderRadius: "1rem",
+						background: `linear-gradient(white, white) padding-box, 
+                 linear-gradient(to top right, black ${progressWidth}%, #d4d4d8 ${progressWidth}%) border-box`,
+						transition: "all 0.5s ease",
+					}}
 				/>
 			</div>
 		</div>
