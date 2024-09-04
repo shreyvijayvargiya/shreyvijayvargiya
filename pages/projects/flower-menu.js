3138d07c79f84745232ddce8b91b49de93c1036d
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-04 14:59:56 +0530

    flower menu is ready to deploy!!

--- pages/projects/flower-menu.js
+++ pages/projects/flower-menu.js
@@ -34,6 +34,11 @@ const FlowerMenu = () => {
 			duration: 1,
 			stagger: 0.2,
 		});
+		gsap.to(".container", {
+			rotation: isOpen ? "+=720" : "0",
+			duration: 5,
+			ease: "circ",
+		});
 	};
 
 	const numberOfItems = circleItems.length;
@@ -51,13 +56,15 @@ const FlowerMenu = () => {
 					) : (
 						<MenuIcon size={18} color="white" />
 					)}
-					<span className="text-xs text-gray-100 group-hover:inline-block group-hover:ml-2 hidden">
+					<span
+						className={`text-xs text-gray-100 group-hover:block group-hover:ml-2 hidden transition-transform duration-300 transform`}
+					>
 						{isOpen ? "Click to Close" : "Click to Expand"}
 					</span>
 				</div>
 
 				<div
-					className={`container absolute flex justify-center items-center w-full h-full border-2 p-10 rounded-full`}
+					className={`container absolute flex justify-center items-center w-full h-full p-10 rounded-full bg-gradient-to-r from-gray-50 via-purple-300 to-pink-50`}
 				>
 					{circleItems.map((item, index) => (
 						<div
@@ -71,8 +78,8 @@ const FlowerMenu = () => {
 							}}
 						>
 							<div className="bg-gray-900 p-2 rounded-md hover:bg-gray-800 group hover:flex">
-								<item.Icon size={24} color="white" />
-								<span className="text-gray-100 group-hover:inline-block group-hover:ml-2 hidden">
+								<item.Icon size={20} color="white" />
+								<span className="text-sm text-gray-100 group-hover:inline-block group-hover:ml-2 hidden duration-200 ease-in transition-all">
 									{item.label}
 								</span>
 							</div>
