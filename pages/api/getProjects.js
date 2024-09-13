26e989faf0a5fc3379465a9f900c85d327038853
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-13 15:05:04 +0530

    al! good!!

--- pages/api/getProjects.js
+++ pages/api/getProjects.js
@@ -3,7 +3,6 @@ import path from "path";
 
 const getProjectsApi = async (req, res) => {
 	const dirPath = path.join(process.cwd(), "/pages/projects");
-	console.log(dirPath, "dirPath");
 	const projectFiles = fs.readdirSync(dirPath, { recursive: true });
 	const files = projectFiles?.map((item, index) => {
 		return {
