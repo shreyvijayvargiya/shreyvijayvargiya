c25ed5ae0c79af3e04f9490c7665aca62a5ae905
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-19 18:24:46 +0530

    all good to release stack-over

--- pages/projects/stack-over.js
+++ pages/projects/stack-over.js
@@ -40,7 +40,7 @@ const StackOver = () => {
 					);
 				})}
 			</div>
-			<div className="fixed bottom-10">
+			<div className="fixed bottom-5">
 				<button
 					onClick={() => setShow(!show)}
 					className="px-4 py-2 my-1 bg-gray-800 hover:bg-black text-white rounded-md w-full"
