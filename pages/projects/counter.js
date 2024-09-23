c4e639b57f1794a45c076f3932725ba667041e16
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-23 19:20:59 +0530

    counter is also created!!

--- pages/projects/counter.js
+++ pages/projects/counter.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
 import { Plus, Minus } from "lucide-react";
 import { gsap } from "gsap";
 
@@ -7,48 +6,82 @@ const Counter = () => {
 	const [count, setCount] = useState(0);
 
 	const handleDecrease = () => {
+		gsap.fromTo(".minus-button", { scale: 0.7 }, { scale: 1, ease: "elastic" });
 		if (count > 0) {
-			// Animate the text up
-			gsap.to(".counter-text", {
-				y: -40,
-				duration: 0.3,
-				onComplete: () => {
-					setCount(count - 1);
-					gsap.to(".counter-text", { y: 0, duration: 0.3 });
+			gsap.fromTo(
+				".counter-text",
+				{
+					x: 0,
+					opacity: 1,
+					scale: 0,
 				},
-			});
+				{
+					opacity: 0,
+					scale: 1,
+					duration: 0.3,
+					x: -100,
+					ease: "power1",
+					onComplete: () => {
+						setCount(count - 1);
+						gsap.fromTo(
+							".counter-text",
+							{ x: 100, opacity: 0, scale: 0 },
+							{ x: 0, opacity: 1, scale: 1, duration: 0.3 }
+						);
+					},
+				}
+			);
 		}
 	};
 
 	const handleIncrease = () => {
-		// Animate the text down
-		gsap.to(".counter-text", {
-			y: 40,
-			duration: 0.3,
-			onComplete: () => {
-				setCount(count + 1);
-				gsap.to(".counter-text", { y: 0, duration: 0.3 });
+		gsap.fromTo(".plus-button", { scale: 0.7 }, { scale: 1, ease: "elastic" });
+		gsap.fromTo(
+			".counter-text",
+			{
+				x: 0,
+				opacity: 1,
+				scale: 1,
 			},
-		});
+			{
+				opacity: 0,
+				scale: 0,
+				duration: 0.3,
+				x: 100,
+				onComplete: () => {
+					setCount(count + 1);
+					gsap.fromTo(
+						".counter-text",
+						{ x: -100, opacity: 0, scale: 0 },
+						{ x: 0, opacity: 1, scale: 1, duration: 0.3 }
+					);
+				},
+			}
+		);
 	};
 
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			<motion.div className="flex items-center space-x-4">
+			<div>
+				<p className="font-display counter-text text-7xl font-semibold">
+					{count}
+				</p>
+			</div>
+			<div className="flex items-center space-x-20 my-10">
 				<button
 					onClick={handleDecrease}
-					className="bg-red-500 text-white p-2 rounded"
+					className="minus-button bg-gray-200 text-gray-400 rounded-full p-2"
 				>
 					<Minus />
 				</button>
-				<div className="counter-text text-4xl">{count}</div>
+
 				<button
 					onClick={handleIncrease}
-					className="bg-green-500 text-white p-2 rounded"
+					className="plus-button bg-gray-200 text-gray-400 rounded-full p-2"
 				>
 					<Plus />
 				</button>
-			</motion.div>
+			</div>
 		</div>
 	);
 };
