59df99561029d458b63a2c02be8f8a1219e0870e
shreyvijayvargiya <shreyvijayvargiya26@gmail.com>
2024-09-26 01:49:01 +0530

    clock digital created!!

--- pages/projects/music-card.js
+++ pages/projects/music-card.js
@@ -9,11 +9,9 @@ const MusicCard = () => {
 			<div className="relative bg-gray-100 cursor-pointer rounded-xl w-96 h-96 z-50 border border-gray-200 group overflow-hidden">
 				<div
 					className={`${classes.cdCard} cd-card absolute top-0 left-0 right-0 bottom-0 bg-gray-300 z-10 border border-gray-200`}
-				>
-					
-				</div>
+				/>
 				<div className="w-full h-full mt-20 relative flex justify-center flex-col z-0 items-center p-4">
-          <Music2Icon className="animate-bounce" />
+					<Music2Icon className="animate-bounce" />
 					<p className="text-gray-400 text-center">Currently listening</p>
 					<p className="text-xl text-center">Lex Fridmand with Donald Trump</p>
 				</div>
@@ -32,10 +30,10 @@ const useStyles = createStyles((theme) => ({
 		transition: "all 0.8s ease-in-out",
 		borderRadius: "0.75rem",
 		backgroundImage: "url(/demo-images/img-4.avif)",
-    imageOrientation: "center center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundOrigin: "content-box",
+		imageOrientation: "center center",
+		backgroundSize: "cover",
+		backgroundRepeat: "no-repeat",
+		backgroundOrigin: "content-box",
 		"&:hover": {
 			height: "100%",
 			border: `1px solid ${theme.colors.gray[3]}`,
